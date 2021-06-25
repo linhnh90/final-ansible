ae410338e9f2b2bcd90e355f0350991aac611975
linhnh90 <linh.nh@medofa.com>
2021-06-25 21:29:31 +0700

    ansible LAMP

---
+++ roles/mysql/files/dump.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS student (           
+  name varchar(255)       
+  );         
+  INSERT INTO student(name) VALUES('linh');
+  INSERT INTO student(name) VALUES('Quang');         
